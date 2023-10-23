7620df9dc5b8a80204247844ab908da9c36699af
Heejae Kwon <sfhy33@gmail.com>
2023-10-23 14:12:02 +0900

    add strawberry api

---
+++ strawberry/lib/dataset/__init__.py
@@ -0,0 +1,17 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft
+# Licensed under the MIT License.
+# Written by Bin Xiao (Bin.Xiao@microsoft.com)
+# ------------------------------------------------------------------------------
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+#from .mpii import MPIIDataset as mpii
+#from .coco import COCODataset as coco
+#from .deepfashion2 import DeepFashion2Dataset as deepfashion2
+
+from .straw_try import Straw_Try as straw_try
+# from .keypoint_strawberry import keypointsStraw_Try as keypoints_strawberry
+#from .drawer_keypoint import drawer_keypoint
